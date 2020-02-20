2babd44e894732748d0dd83998e6356ca6a8b2a0
tarbielee <tarbieletsoalo@gmail.com>
2020-02-20 13:42:17 +0200

    refactoredCode

--- spec/node_spec.js
+++ spec/node_spec.js
@@ -1,24 +1,30 @@
 let Visitor = require("../src/nodeio");
-let visitor = new Visitor("alice", 19, "05/07/19", "11Oclock", "jolly", "John Greek" );
+let visitor = new Visitor(
+  "alice",
+  19,
+  "05/07/19",
+  "11Oclock",
+  "jolly",
+  "John Greek"
+);
 
-
-describe("fullName", function () {
-  it("should expect fullName", function () {
+describe("Visitor", function() {
+  it("should expect fullName", function() {
     expect(visitor.fullName).toBe("alice");
   });
-  it("it should expect age", function () {
+  it("should expect age", function() {
     expect(visitor.age).toBe(19);
   });
-  it("it should expect dateOfvisit", function () {
+  it("should expect dateOfvisit", function() {
     expect(visitor.dateOfVisit).toBe("05/07/19");
   });
-  it("it should expect timeOfVisit", function () {
+  it("should expect timeOfVisit", function() {
     expect(visitor.timeOfVisit).toBe("11Oclock");
   });
-  it("it should expect comments", function () {
+  it("should expect comments", function() {
     expect(visitor.comments).toBe("jolly");
   });
-  it("it should expect assistor", function () {
+  it("should expect assistor", function() {
     expect(visitor.assistor).toBe("John Greek");
   });
-})
+});
--- src/nodeio.js
+++ src/nodeio.js
@@ -1,46 +1,59 @@
-"use strict"
+"use strict";
 
-let fs = require('fs')
+let fs = require("fs");
 
 class Visitor {
-    constructor(fullName, age, dateOfVisit, timeOfVisit, comments, assistor) {
-        this.fullName = fullName,
-        this.age = age,
-        this.dateOfVisit = dateOfVisit,
-        this.timeOfVisit = timeOfVisit,
-        this.comments = comments,
-        this.assistor = assistor;
-    }
+  constructor(fullName, age, dateOfVisit, timeOfVisit, comments, assistor) {
+    (this.fullName = fullName),
+      (this.age = age),
+      (this.dateOfVisit = dateOfVisit),
+      (this.timeOfVisit = timeOfVisit),
+      (this.comments = comments),
+      (this.assistor = assistor);
+  }
 
-    save() {
-        fs.writeFile(`visitor_${this.fullName}.json`, JSON.stringify(this, null, 4),
-        (err) => {
-            if (err) throw err;
-            console.log('File saved');
-          });
-    }
+  save() {
+    fs.writeFile(
+      `visitor_${this.fullName}.json`,
+      JSON.stringify(this, null, 4),
+      err => {
+        if (err) throw err;
+        console.log("File saved");
+      }
+    );
+  }
 
-    load() {
-        fs.readFile(`visitor_${this.fullName}.json`, "utf8",
-        (err, info) => {
-            if (err) { 
-                throw err;}
-              else
-                console.log(info)
-              
-              
-          });
-    }
+  load() {
+    fs.readFile(`visitor_${this.fullName}.json`, "utf8", (err, info) => {
+      if (err) {
+        throw err;
+      } else console.log(info);
+    });
+  }
 }
-let alice = new Visitor("alice", 19, "05/07/19", "9Oclock", "jolly", "John Greek" );
-let bob = new Visitor("bob", 15, "10/08/19", "11Oclock", "fantastic", "Mmabatho Betha" );
-let charlie = new Visitor("charlie", 26, "18/11/19", "7Oclock", "was great", "Lion Berwanger" );
-
-
-console.log(alice.save())
-console.log(bob.save())
-console.log(charlie.save())
-console.log(alice.load())
-console.log(bob.load())
+let alice = new Visitor(
+  "alice",
+  19,
+  "05/07/19",
+  "9Oclock",
+  "jolly",
+  "John Greek"
+);
+let bob = new Visitor(
+  "bob",
+  15,
+  "10/08/19",
+  "11Oclock",
+  "fantastic",
+  "Mmabatho Betha"
+);
+let charlie = new Visitor(
+  "charlie",
+  26,
+  "18/11/19",
+  "7Oclock",
+  "was great",
+  "Lion Berwanger"
+);
 
 module.exports = Visitor;
