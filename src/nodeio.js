73b59e8312f3defa20740977269d11490b4dd902
tarbielee <tarbieletsoalo@gmail.com>
2020-02-20 13:29:28 +0200

    first commit

--- src/nodeio.js
+++ src/nodeio.js
@@ -1,22 +1,46 @@
 "use strict"
 
+let fs = require('fs')
+
 class Visitor {
-    constructor(fullName, age, dateOfVisit, timeOfVisit, comments, nameOfThePersonWhoAssistedTheVisitor) {
+    constructor(fullName, age, dateOfVisit, timeOfVisit, comments, assistor) {
         this.fullName = fullName,
         this.age = age,
         this.dateOfVisit = dateOfVisit,
         this.timeOfVisit = timeOfVisit,
         this.comments = comments,
-        this.nameOfThePersonWhoAssistedTheVisitor = nameOfThePersonWhoAssistedTheVisitor;
+        this.assistor = assistor;
     }
 
     save() {
-        if(fs.readFile(visitor_$their_full_name$.json)){
-            return fileExists
-        }
+        fs.writeFile(`visitor_${this.fullName}.json`, JSON.stringify(this, null, 4),
+        (err) => {
+            if (err) throw err;
+            console.log('File saved');
+          });
+    }
 
-        else(fs.writeFile(visitor_$their_full_name$.json), function(err){
-            if (err) throw(err)
-        })
+    load() {
+        fs.readFile(`visitor_${this.fullName}.json`, "utf8",
+        (err, info) => {
+            if (err) { 
+                throw err;}
+              else
+                console.log(info)
+              
+              
+          });
     }
 }
+let alice = new Visitor("alice", 19, "05/07/19", "9Oclock", "jolly", "John Greek" );
+let bob = new Visitor("bob", 15, "10/08/19", "11Oclock", "fantastic", "Mmabatho Betha" );
+let charlie = new Visitor("charlie", 26, "18/11/19", "7Oclock", "was great", "Lion Berwanger" );
+
+
+console.log(alice.save())
+console.log(bob.save())
+console.log(charlie.save())
+console.log(alice.load())
+console.log(bob.load())
+
+module.exports = Visitor;
